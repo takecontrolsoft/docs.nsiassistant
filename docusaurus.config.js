d37280b396ebc491cb286d160c5e0ff5e339f87f
Desislava Stefaanova <dst.stefanova@gmail.com>
2024-04-23 01:36:40 +0300

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,141 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'НСИ асистент',
+  tagline: 'Помощник за подаване на годишни отчети към Националния статистически институт',
+  favicon: 'img/favicon.png',
+
+  // Set the production url of your site here
+  url: 'https://nsiwebassistant.azurewebsites.net/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'takecontrolsoft', // Usually your GitHub org/user name.
+  projectName: 'nsi_assistant_docs', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'bg',
+    locales: ['bg'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/takecontrolsoft/nsi_assistant_docs/tree/main/docs/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/takecontrolsoft/nsi_assistant_docs/tree/main/blog/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/nsi-card.png',
+      navbar: {
+        title: 'НСИ асистент',
+        logo: {
+          alt: 'НСИ асистент лого',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Указания',
+          },
+          {to: '/blog', label: 'Блог', position: 'left'},
+          {
+            href: 'https://github.com/takecontrolsoft/nsi_assistant_docs',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Описание',
+            items: [
+              {
+                label: 'Указания',
+                to: '/docs/intro',
+              },
+              {
+                label: 'Инсталиране',
+                to: '/docs/install',
+              },
+            ],
+          },
+          {
+            title: 'Връзки',
+            items: [
+              {
+                label: 'Take control - SI',
+                href: 'https://takecontrolsoft.eu/',
+              },
+              {
+                label: 'Linked In',
+                href: 'https://www.linkedin.com/company/take-control-si',
+              },
+            ],
+          },
+          {
+            title: 'Разработка',
+            items: [
+              {
+                label: 'Новости',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/takecontrolsoft',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} NSI Assistant, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
