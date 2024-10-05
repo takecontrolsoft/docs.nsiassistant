e73238319c4e85fcce9de1b073c05260b6e793b9
Desislava Stefaanova <dst.stefanova@gmail.com>
2024-10-05 22:01:39 +0300

    changed domain names

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://doc.nsiassistant.bg/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/doc.nsiassistant.bg',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
