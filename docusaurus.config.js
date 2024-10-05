b25141c79f08f46df800343eda707a81c156a960
Desislava Stefaanova <dst.stefanova@gmail.com>
2024-10-05 22:00:58 +0300

    domain names

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://nsiassistant.bg/',
+  url: 'https://doc.nsiassistant.bg/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs.nsiassistant',
+  baseUrl: '/doc.nsiassistant.bg',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
